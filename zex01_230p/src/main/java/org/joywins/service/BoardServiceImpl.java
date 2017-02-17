6c3b6c9fdaae04a30c3c4c585aea98d94552c174
tudoistube <tudoistube@ymail.com>
2017-02-17 12:08:58 +0900

    master

---
+++ zex01_230p/src/main/java/org/joywins/service/BoardServiceImpl.java
@@ -0,0 +1,44 @@
+package org.joywins.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.joywins.dao.IF_BoardDAO;
+import org.joywins.domain.BoardVO;
+import org.springframework.stereotype.Service;
+
+//...188p.@Service가 스프링의 빈으로 인식하게 함. root-context.xml::Beans Graph 확인할 것.
+@Service
+public class BoardServiceImpl implements IF_BoardService {
+
+	@Inject
+	private IF_BoardDAO dao;
+
+	
+	@Override
+	public void insert(BoardVO board) throws Exception {
+		dao.insert(board);
+	}
+
+	@Override
+	public BoardVO read(Integer bno) throws Exception {
+	    return dao.read(bno);
+	}
+
+	@Override
+	public void update(BoardVO board) throws Exception {
+	    dao.update(board);
+	}
+
+	@Override
+	public void delete(Integer bno) throws Exception {
+	    dao.delete(bno);
+	}
+
+	@Override
+	public List<BoardVO> listAll() throws Exception {
+		return dao.listAll();
+	}
+
+}

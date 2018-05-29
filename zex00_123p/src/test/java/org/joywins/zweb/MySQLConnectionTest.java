d529593223a95c3a8ff413ced239da15adeaef7d
hanmydog <hanmydog@chol.com>
2018-05-30 00:19:07 +0900

    74p.test MySqlConnection

--- zex00_123p/src/test/java/org/joywins/zweb/MySQLConnectionTest.java
+++ zex00_123p/src/test/java/org/joywins/zweb/MySQLConnectionTest.java
@@ -8,13 +8,19 @@ import org.junit.Test;
 public class MySQLConnectionTest {
 
 	private static final String DRIVER = 
-			"com.mysql.jdbc.Driver";
+			"com.mysql.cj.jdbc.Driver";
+/*
+*..MySQL JDBC 드라이버 Time Zone 이슈	
+	https://desty.github.io/2016/08/14/mysql-jdbc-timezone-issue
+*...MySQL – Establishing SSL connection without server’s identity verification is not recommended	
+	https://www.mkyong.com/mysql/mysql-establishing-ssl-connection-without-servers-identity-verification-is-not-recommended/
+*/			
 	private static final String URL = 
-			"jdbc:mysql://127.0.0.1:3306/zbook_ex";
+			"jdbc:mysql://127.0.0.1:3306/zbook_ex?serverTimezone=UTC&useSSL=false";
 	private static final String USER = 
-			"JoyWins";
+			"*****";
 	private static final String PW = 
-			"ZSql@";	
+			"*****";	
 	
 	@Test
 	public void testConnection() throws Exception {
